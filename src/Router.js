0da82664033e6a266995cb6d78745dc7f55d875a
Jacoblynch99 <jacoblynch99@gmail.com>
2020-12-02 18:18:36 -0600

    initial commit

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router'
 import cookie from 'cookie'
-import Listings from './containers/Listings'
-import Login from './containers/Login'
-import Restaurant from './containers/Restaurant'
-import AddListing from './containers/AddListing'
+import Hello from './componenets/Hello'
 
 const Router = () => {
     const checkAuth = () => {
@@ -30,7 +27,7 @@ const Router = () => {
 
     return (
         <Switch>
-            {/* ROUTES HERE */}
+            <Route exact path="/" component={Hello} />
         </Switch>
     )
 }
---
+++ src/componenets/Hello.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Hello = (props) => {
+    return <h1>Hello World</h1>
+}
+
+export default Hello
